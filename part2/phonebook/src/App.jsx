8957bf32d2aba2119734e683887274dea4b9f37a
wagany <wanderingganymede@protonmail.com>
2025-10-17 17:10:11 +0200

    part2-c done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,7 @@
+import axios from "axios";
 import { useState } from "react";
+import { useEffect } from "react";
+
 const Filter = ({ searchString, searchInputChange }) => {
   return (
     <>
@@ -55,12 +58,7 @@ const Persons = ({ persons, searchString }) => {
   );
 };
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchString, setNewSearchString] = useState("");
@@ -88,6 +86,14 @@ const App = () => {
     setNewSearchString(event.target.value);
   };
 
+  const hook = () => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("response data", response.data);
+      setPersons(response.data);
+    });
+  };
+
+  useEffect(hook, []);
   return (
     <div>
       <h2>Phonebook</h2>
